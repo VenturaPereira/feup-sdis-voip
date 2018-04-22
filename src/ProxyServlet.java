02f3bbfa5e669811f92e4eaa97984f4b10c5565a
aquelemiguel <miguelmanofernandes@gmail.com>
2018-04-22 16:07:51 +0100

    Basic proxy setup

--- src/ProxyServlet.java
+++ src/ProxyServlet.java
@@ -6,6 +6,22 @@ public class ProxyServlet extends SipServlet implements SipErrorListener, Servle
     /** Creates a new instance of ProxyServlet. */
     public ProxyServlet() {}
 
+    @Override
+    protected void doInvite(SipServletRequest request) {
+        System.out.println("Got REQUEST:\n" + request);
+    }
+
+    @Override
+    protected void doBye(SipServletRequest request) {
+        System.out.println("Got BYE:\n" + request);
+        super.doBye(request);
+    }
+
+    @Override
+    protected void doResponse(SipServletResponse response) {
+        System.out.println("Got RESPONSE:\n" + response);
+    }
+
     /** SIP error listeners. */
     @Override
     public void noAckReceived(SipErrorEvent e) {
