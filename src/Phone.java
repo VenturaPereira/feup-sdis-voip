1f4f737f9a9a794a2f38013f7a12be6be8395d24
aquelemiguel <miguelmanofernandes@gmail.com>
2018-04-24 01:44:03 +0100

    Simple client-proxy communication

--- src/Phone.java
+++ src/Phone.java
@@ -1,7 +1,43 @@
-public class Phone{
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+import java.net.UnknownHostException;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.SocketException;
 
-    public Phone(){
-        System.out.println("Ringing...");
+public class Phone {
+
+    private String host_name;
+    private int port;
+    private DatagramSocket socket;
+
+    public static void main(String[] args) throws IOException {
+        Phone phone = new Phone(args[0], Integer.parseInt(args[1]));
+
+        phone.display_connection_info();
+        phone.connect_to_proxy();
+    }
+
+    public Phone(String host_name, int port) throws SocketException {
+        this.host_name = host_name;
+        this.port = port;
+        this.socket = new DatagramSocket();
+    }
+
+    public void connect_to_proxy() throws IOException {
+        InetAddress addr = InetAddress.getByName(this.host_name);
+
+        byte[] handshake_msg = new String("Hello").getBytes();
+
+        DatagramPacket handshake = new DatagramPacket(handshake_msg, handshake_msg.length, addr, this.port);
+        this.socket.send(handshake);
+    }
+    
+    public void display_connection_info() {
+        System.out.println("\nSetting up proxy server...");
+        System.out.format("• Proxy hostname: %s\n", this.host_name);
+        System.out.format("• Proxy port: %d\n\n", this.port);
+        System.out.println("Connecting to proxy server...\n");
     }
 
 }
---
+++ src/ProxyServer.java
@@ -0,0 +1,30 @@
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+import java.net.SocketException;
+import java.io.IOException;
+
+public class ProxyServer {
+
+    private DatagramSocket socket;
+
+    public static void main(String[] args) throws IOException {
+        ProxyServer proxy = new ProxyServer(Integer.parseInt(args[0]));
+
+        proxy.read_socket();
+    }
+
+    public ProxyServer(int port) throws SocketException {
+        this.socket = new DatagramSocket(port);
+    }
+
+    public void read_socket() throws IOException {
+        
+        while (true) {
+            DatagramPacket packet = new DatagramPacket(new byte[512], 512);
+            this.socket.receive(packet);
+
+            System.out.format("%s\n", new String(packet.getData()));
+        }
+    }
+
+}
