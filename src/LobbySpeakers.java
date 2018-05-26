f2f8ae546d098edfb6773d068d5b17a2d7e76f16
VenturaPereira <venturasousapereira@hotmail.com>
2018-05-26 18:55:59 +0100

    ILS SONT LE CHAMPIOONS

--- src/LobbySpeakers.java
+++ src/LobbySpeakers.java
@@ -54,7 +54,7 @@ public class LobbySpeakers implements Runnable{
 
      public void run()
     {
-     //   try{
+        try{
         System.out.println("Server started at port:" + this.port);
         byte[] receiveData = new byte[1024];
         try(MulticastSocket serverSocket = new MulticastSocket(this.port)){
@@ -66,39 +66,40 @@ public class LobbySpeakers implements Runnable{
 
         
 
-        //Mixer.Info[] mixer_info = AudioSystem.getMixerInfo();
+        Mixer.Info[] mixer_info = AudioSystem.getMixerInfo();
         
-    //    format = new AudioFormat(sampleRate, 16, 2, true, true);
-      //  dataLineInfo = new DataLine.Info(SourceDataLine.class, format);
+      format = new AudioFormat(sampleRate, 16, 2, true, true);
+        dataLineInfo = new DataLine.Info(SourceDataLine.class, format);
 
-        //Mixer mixer = AudioSystem.getMixer(mixer_info[this.out_device]);
-        //sourceDataLine = (SourceDataLine) mixer.getLine(dataLineInfo);
-        //sourceDataLine.open(format);
+        Mixer mixer = AudioSystem.getMixer(mixer_info[this.out_device]);
+        sourceDataLine = (SourceDataLine) mixer.getLine(dataLineInfo);
+        sourceDataLine.open(format);
                       System.out.println("hum");
 
-        //sourceDataLine.start();
+        sourceDataLine.start();
 
         //FloatControl volumeControl = (FloatControl) sourceDataLine.getControl(FloatControl.Type.MASTER_GAIN);
         //volumeControl.setValue(1.00f);
 
 
-        //ByteArrayInputStream baiss = new ByteArrayInputStream(receivePacket.getData());
-
+      
         while (status == true) 
         {
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+              ByteArrayInputStream baiss = new ByteArrayInputStream(receivePacket.getData());
+
             serverSocket.receive(receivePacket);
-            String receiving = new String(receivePacket.getData(), 0, receivePacket.getData().length);
-            System.out.println(receiving);
-           // ais = new AudioInputStream(baiss, format, receivePacket.getLength());
-            //toSpeaker(receivePacket.getData());
+          //  String receiving = new String(receivePacket.getData(), 0, receivePacket.getData().length);
+        //    System.out.println(receiving);
+            ais = new AudioInputStream(baiss, format, receivePacket.getLength());
+            toSpeaker(receivePacket.getData());
         }
 
-        //sourceDataLine.drain();
-        //sourceDataLine.close();
-        //}catch(LineUnavailableException e){
-          //  e.printStackTrace();
-    //}
+        sourceDataLine.drain();
+        sourceDataLine.close();
+        }catch(LineUnavailableException e){
+            e.printStackTrace();
+    }
 
     }catch(Exception e){
             e.printStackTrace();
